8b517521a35eea40912997aa5ee3a3f0096d6c19
Heneros <punk60826@gmail.com>
2021-09-01 23:34:08 +0300

    functions.php updated

--- functions.php
+++ functions.php
@@ -1,16 +1,23 @@
 <?php
 
-function _si_assets_path($path){
+function _ho_assets_path($path){
     return get_template_directory_uri() . '/assets/' . $path;
 }
 
-
-function si_scripts(){
-    wp_enqueue_script('js', _si_assets_path('/js/main.js'), [], '1.0', true);
-    wp_enqueue_style('main-style', _si_assets_path('/css/style.css'), [], '1.0', 'all');
-
+add_action('wp_enqueue_scripts', 'ho_scripts');
+function ho_scripts(){
+    wp_enqueue_script('js', _ho_assets_path('/js/main.js'), [], '1.0', true);
+    wp_enqueue_script('js', _ho_assets_path('/js/script.js'), [], '1.0', true);
+    wp_enqueue_style('main-style', _ho_assets_path('/css/style.css'), [], '1.0', 'all');
 }
-add_action('wp_enqueue_scripts', 'si_scripts');
+
+add_action('after_setup_theme', 'ho_setup');
+function ho_setup(){
+    register_nav_menu('menu-header', 'Menu in header');
+    register_nav_menu('menu-body', 'Menu in body');
+    register_nav_menu('menu in footer', 'Menu in footer');
+}
+
 
 
 ?>
